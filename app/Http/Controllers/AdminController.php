4b7aa1c47c955af1316a4c242e2cfd2fa526d696
RAfi123-cmd <rafiulhuda27@gmail.com>
2024-08-28 16:10:08 +0700

    menambahkan route post pada adminloginsubmit

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,12 +6,16 @@ use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
-    public function AdminLogin(){
+    public function AdminLogin()
+    {
         return view('admin.login');
     }
     // end method
 
-    public function AdminDashboard(){
+    public function AdminDashboard()
+    {
         return view('admin.admin_dashboard');
     }
+
+    public function AdminLoginSubmit(Request $request) {}
 }
