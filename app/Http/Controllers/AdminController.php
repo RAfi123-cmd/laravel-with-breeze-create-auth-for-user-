850e1c668d3f03e73c2a01532c1504dc80d25a90
RAfi123-cmd <rafiulhuda27@gmail.com>
2024-08-29 15:13:32 +0700

    menambahkan mail Websitemail untuk membuat token pada admin di forget password

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\Websitemail;
+use App\Models\Admin;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
 
 class AdminController extends Controller
 {
@@ -57,5 +60,50 @@ class AdminController extends Controller
         return view('admin.forget_password');
     }
 
-    public function AdminPasswordSubmit(Request $request) {}
+    // public function AdminPasswordSubmit(Request $request)
+    // {
+    //     $request->validate([
+    //         'email' => 'required|email'
+    //     ]);
+    //     $admin_data = Admin::where('email', $request->email)->first();
+    //     if ($admin_data) {
+    //         return redirect()->back()->with('error', 'Email Not Found');
+    //     }
+    //     $token = hash('sha256', time());
+    //     $admin_data->token = $token;
+    //     $admin_data->update();
+
+    //     $reset_link = url('admin/reset-password/' . $token . '/' . $request->email);
+    //     $subject = "Reset Password";
+    //     $message = "Please Clink on below link to reset password<br>";
+    //     $message .= "<a href='" . $reset_link . " ' > Click Here </a>";
+
+    //     Mail::to($request->email)->send(new Websitemail($subject, $message));
+    //     return redirect()->back()->with('success', 'Reset Password Link Send On Your Email');
+    // }
+
+    public function AdminPasswordSubmit(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email'
+        ]);
+
+        $admin_data = Admin::where('email', $request->email)->first();
+        if (!$admin_data) {  // Memeriksa apakah admin_data tidak ditemukan
+            return redirect()->back()->with('error', 'Email Not Found');
+        }
+
+        $token = hash('sha256', time());
+        $admin_data->token = $token;
+        $admin_data->update();
+
+        $reset_link = url('admin/reset-password/' . $token . '/' . $request->email);
+        $subject = "Reset Password";
+        $message = "Please Clink on below link to reset password<br>";
+        $message .= "<a href='" . $reset_link . "'> Click Here </a>";
+
+        Mail::to($request->email)->send(new Websitemail($subject, $message));
+
+        return redirect()->back()->with('success', 'Reset Password Link Send On Your Email');
+    }
 }
