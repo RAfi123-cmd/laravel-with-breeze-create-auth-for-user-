7c4839243a856831ab7fff3364a0833d8eceabc2
RAfi123-cmd <rafiulhuda27@gmail.com>
2024-08-31 15:50:46 +0700

    menambahkan route get dan controller dan views pada Admin reset password

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -7,6 +7,7 @@ use App\Models\Admin;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Mail;
 
 class AdminController extends Controller
@@ -38,15 +39,6 @@ class AdminController extends Controller
         } else {
             return redirect()->route('admin.login')->with('error', 'Invalid Creadentials');
         }
-        // $credentials = $request->only('email', 'password');
-
-        // if (Auth::guard('admin')->attempt($credentials)) {
-        //     // Authentication passed
-        //     return redirect()->route('admin.dashboard')->with('success', 'Login successful');
-        // }
-
-        // // Authentication failed
-        // return redirect()->route('admin.login')->with('error', 'Invalid credentials');
     }
 
     public function AdminLogout()
@@ -59,28 +51,6 @@ class AdminController extends Controller
     {
         return view('admin.forget_password');
     }
-
-    // public function AdminPasswordSubmit(Request $request)
-    // {
-    //     $request->validate([
-    //         'email' => 'required|email'
-    //     ]);
-    //     $admin_data = Admin::where('email', $request->email)->first();
-    //     if ($admin_data) {
-    //         return redirect()->back()->with('error', 'Email Not Found');
-    //     }
-    //     $token = hash('sha256', time());
-    //     $admin_data->token = $token;
-    //     $admin_data->update();
-
-    //     $reset_link = url('admin/reset-password/' . $token . '/' . $request->email);
-    //     $subject = "Reset Password";
-    //     $message = "Please Clink on below link to reset password<br>";
-    //     $message .= "<a href='" . $reset_link . " ' > Click Here </a>";
-
-    //     Mail::to($request->email)->send(new Websitemail($subject, $message));
-    //     return redirect()->back()->with('success', 'Reset Password Link Send On Your Email');
-    // }
 
     public function AdminPasswordSubmit(Request $request)
     {
@@ -105,5 +75,15 @@ class AdminController extends Controller
         Mail::to($request->email)->send(new Websitemail($subject, $message));
 
         return redirect()->back()->with('success', 'Reset Password Link Send On Your Email');
+    }
+
+    public function AdminResetPassword ($token, $email){
+        $admin_data = Admin::where('email', $email)->where('token', $token)->first();
+
+        if (!$admin_data) {
+            return redirect()->route('admin.login')->with('error', 'Invalid Token or Email');
+        }
+        return view('admin.reset_password', compact('token', 'email'));
+
     }
 }
