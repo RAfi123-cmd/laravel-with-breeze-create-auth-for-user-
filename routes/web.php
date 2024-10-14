03675b508db3ddd10f714ee8b98dda9d605c5f69
RAfi123-cmd <rafiulhuda27@gmail.com>
2024-10-14 23:06:35 +0700

    Frontend Template Setup Part 1

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,15 @@
 
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\ClientController;
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [UserController::class, 'Index'])->name('index');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
