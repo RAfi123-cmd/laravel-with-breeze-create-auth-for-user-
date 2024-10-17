3bc505054610930d159c26d5ee33e1ae0a111978
RAfi123-cmd <rafiulhuda27@gmail.com>
2024-10-17 16:07:18 +0700

    Update User Login Setup with and without Login

--- resources/views/frontend/layouts/header.blade.php
+++ resources/views/frontend/layouts/header.blade.php
@@ -18,6 +18,9 @@
                </a>
             </li>
 
+            
+
+            @auth
             @php
             $id = Auth::user()->id;
             $profileData = App\Models\User::find($id);
@@ -33,6 +36,19 @@
                   <a class="dropdown-item" href="{{ route('user.logout') }}"><i class="icofont-sale-discount"></i> Logout</a>
                </div>
             </li>
+            @else
+            <li class="nav-item dropdown">
+               <a class="nav-link" href="{{ route('login') }}" role="button"  aria-haspopup="true" aria-expanded="false">
+                  Login
+               </a>
+            </li>
+            <li class="nav-item dropdown">
+               <a class="nav-link" href="{{ route('register') }}" role="button" aria-haspopup="true" aria-expanded="false">
+                  Register
+               </a>
+            </li>
+            @endauth
+            
             <li class="nav-item dropdown dropdown-cart">
                <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                   <i class="fas fa-shopping-basket"></i> Cart
