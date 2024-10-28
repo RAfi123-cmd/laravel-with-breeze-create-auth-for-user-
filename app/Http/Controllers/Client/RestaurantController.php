86214bb3d47fa95a5a8e416163205c23914e32db
RAfi123-cmd <rafiulhuda27@gmail.com>
2024-10-28 15:11:11 +0700

    Restaurant Backend Menu Option Part 1

---
+++ app/Http/Controllers/Client/RestaurantController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Client;
+
+use App\Http\Controllers\Controller;
+use App\Models\Menu;
+use Illuminate\Http\Request;
+
+class RestaurantController extends Controller
+{
+    public function AllMenu(){
+        $menu = Menu::latest()->get();
+        return view('client.backend.menu.all_menu', compact('menu'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\AdminController;
 use App\Http\Controllers\ClientController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Client\RestaurantController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -81,4 +82,15 @@ Route::middleware('admin')->group(function () {
     }); 
     // end admin middleware
     
-}); 
+});
+
+Route::middleware('client')->group(function () {
+    Route::controller(RestaurantController::class)->group(function (){
+        Route::get('/all/menu', 'AllMenu')->name('all.menu');
+        Route::get('/add/category', 'AddCategory')->name('add.category');
+        Route::post('/store/city', 'StoreCity')->name('city.store');
+        Route::get('/edit/city/{id}', 'EditCity');
+        Route::post('/update/city', 'UpdateCity')->name('city.update');
+        Route::get('/delete/city/{id}', 'DeleteCity')->name('delete.city');
+    });
+});
