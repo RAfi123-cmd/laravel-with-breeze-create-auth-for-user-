cb44fd9a61a2edea8ffb8e8e1a846d2acafe4ecc
RAfi123-cmd <rafiulhuda27@gmail.com>
2024-10-31 20:18:29 +0700

    Restaurant Backend Menu Option Part 3

--- app/Http/Controllers/Client/RestaurantController.php
+++ app/Http/Controllers/Client/RestaurantController.php
@@ -3,24 +3,67 @@
 namespace App\Http\Controllers\Client;
 
 use App\Http\Controllers\Controller;
-use App\Models\Menu;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Category;
 use Intervention\Image\ImageManager;
 use Intervention\Image\Drivers\Gd\Driver;
-
+use App\Models\Menu;
 
 class RestaurantController extends Controller
 {
-    public function AllMenu(){
+    public function AllMenu()
+    {
         $menu = Menu::latest()->get();
         return view('client.backend.menu.all_menu', compact('menu'));
     }
-    public function AddMenu(){
+    // End Method 
+
+    public function AddMenu()
+    {
+
         return view('client.backend.menu.add_menu');
     }
-
+    // End Method 
     public function StoreMenu(Request $request)
     {
+        if ($request->file('image')) {
+            $image = $request->file('image');
+            $manager = new ImageManager(new Driver());
+            $name_gen = hexdec(uniqid()) . '.' . $image->getClientOriginalExtension();
+            $img = $manager->read($image);
+            $img->resize(300, 300)->save(public_path('upload/menu/' . $name_gen));
+            $save_url = 'upload/menu/' . $name_gen;
+            Menu::create([
+                'menu_name' => $request->menu_name,
+                'image' => $save_url,
+            ]);
+        }
+        $notification = array(
+            'message' => 'Menu Inserted Successfully',
+            'alert-type' => 'success'
+        );
+        return redirect()->route('all.menu')->with($notification);
+    }
+    // End Method 
+
+    public function EditMenu($id)
+    {
+        $menu = Menu::find($id);
+        return view('client.backend.menu.edit_menu', compact('menu'));
+    }
+    // End Method 
+    public function UpdateMenu(Request $request)
+    {
+        $menu_id = $request->id;
+        $menu = Menu::find($menu_id);
+
+        // if (!$menu) {
+        //     return redirect()->route('all.menu')->with([
+        //         'message' => 'Menu not found',
+        //         'alert-type' => 'error'
+        //     ]);
+        // }
 
         if ($request->file('image')) {
             $image = $request->file('image');
@@ -30,18 +73,37 @@ class RestaurantController extends Controller
             $img->resize(300, 300)->save(public_path('upload/menu/' . $name_gen));
             $save_url = 'upload/menu/' . $name_gen;
 
-            Menu::create([
+            $menu->update([
                 'menu_name' => $request->menu_name,
                 'image' => $save_url,
+            ]);
+        } else {
+            $menu->update([
+                'menu_name' => $request->menu_name,
             ]);
         }
 
         $notification = array(
-            'message' => 'Menu Inserted Successfully',
+            'message' => 'Menu Updated Successfully',
             'alert-type' => 'success'
         );
 
         return redirect()->route('all.menu')->with($notification);
     }
-    // End Method
+
+
+    public function DeleteMenu($id)
+    {
+        $item = Menu::find($id);
+        $img = $item->image;
+        unlink($img);
+        Menu::find($id)->delete();
+        $notification = array(
+            'message' => 'Menu Delete Successfully',
+            'alert-type' => 'success'
+        );
+        return redirect()->back()->with($notification);
+    }
+    // End Method 
+
 }
