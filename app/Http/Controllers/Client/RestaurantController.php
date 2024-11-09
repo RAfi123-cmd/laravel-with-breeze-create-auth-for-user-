b297cc15b392487d634fa4f18f4011a140c9edf1
RAfi123-cmd <rafiulhuda27@gmail.com>
2024-11-09 23:21:48 +0700

    Restaurant Product Option Setup Part 7

--- app/Http/Controllers/Client/RestaurantController.php
+++ app/Http/Controllers/Client/RestaurantController.php
@@ -162,5 +162,15 @@ class RestaurantController extends Controller
         );
         return redirect()->route('all.product')->with($notification);
     }
-    // End Method 
+    // End Method
+    
+    public function EditProduct($id)
+    {
+        $category = Category::latest()->get();
+        $city = City::latest()->get();
+        $menu = Menu::latest()->get();
+        $product = Product::find($id);
+        return view('client.backend.product.edit_product', compact('category', 'city', 'menu', 'product'));
+    }
+    // End Method
 }
