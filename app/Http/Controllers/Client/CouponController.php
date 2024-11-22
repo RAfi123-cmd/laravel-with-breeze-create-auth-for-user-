786e0a9c29e9730c59f9580c5cf433489abf219d
RAfi123-cmd <rafiulhuda27@gmail.com>
2024-11-23 01:12:58 +0700

    Restaurant Manage Coupon Setup Part 2

--- app/Http/Controllers/Client/CouponController.php
+++ app/Http/Controllers/Client/CouponController.php
@@ -16,4 +16,29 @@ class CouponController extends Controller
         return view('client.backend.coupon.all_coupon', compact('coupon'));
     }
     // End Method 
+    public function AddCoupon()
+    {
+        
+        return view('client.backend.coupon.add_coupon');
+    }
+    // End Method
+    
+    public function StoreCoupon(Request $request)
+    {
+        
+            Coupon::create([
+                'coupon_name' => strtoupper($request->coupon_name) ,
+                'coupon_desc' => $request->coupon_desc,
+                'discount' => $request->discount,
+                'validity' => $request->validity,
+                'client_id' => Auth::guard('client')->id(),
+                'created_at' => Carbon::now(),
+            ]);
+        $notification = array(
+            'message' => 'Coupon Inserted Successfully',
+            'alert-type' => 'success'
+        );
+        return redirect()->route('all.coupon')->with($notification);
+    }
+    // End Method 
 }
--- resources/views/client/body/sidebar.blade.php
+++ resources/views/client/body/sidebar.blade.php
@@ -88,7 +88,7 @@
                         </li>
 
                         <li>
-                            <a href="{{ route('add.gallery') }}">
+                            <a href="{{ route('add.coupon') }}">
                                 <span data-key="t-chat">Add Coupon</span>
                             </a>
                         </li>
