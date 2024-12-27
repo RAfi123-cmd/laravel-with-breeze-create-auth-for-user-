acb2a2eebefbc75c258912594241cff081f4e9b7
RAfi123-cmd <rafiulhuda27@gmail.com>
2024-12-27 13:50:38 +0700

    Admin Backend Product Option Setup Part 4

--- resources/views/admin/backend/product/edit_product.blade.php
+++ resources/views/admin/backend/product/edit_product.blade.php
@@ -22,7 +22,7 @@
             <div class="col-xl-12 col-lg-12">
                 <div class="card">
                     <div class="card-body p-4">
-                        <form id="myForm" action="{{ route('product.update') }}" method="post" enctype="multipart/form-data">
+                        <form id="myForm" action="{{ route('admin.product.update') }}" method="post" enctype="multipart/form-data">
                             @csrf
                             <input type="hidden" name="id" value="{{ $product->id }}">
                             <div class="row">
