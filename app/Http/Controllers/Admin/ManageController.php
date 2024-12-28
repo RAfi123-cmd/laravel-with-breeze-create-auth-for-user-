b40ce78c72f42f1a1074070b56934e4d153a1545
RAfi123-cmd <rafiulhuda27@gmail.com>
2024-12-28 13:35:30 +0700

    Admin Backend Restaurant Manage Setup Part 2

--- app/Http/Controllers/Admin/ManageController.php
+++ app/Http/Controllers/Admin/ManageController.php
@@ -160,6 +160,18 @@ class ManageController extends Controller
         $client = Client::where('status',0)->get();
         return view('admin.backend.restaurant.pending_restaurant',compact('client'));
     }
-
     // End Method
+    
+    public function ApproveRestaurant(){
+        $client = Client::where('status',1)->get();
+        return view('admin.backend.restaurant.approve_restaurant',compact('client'));
+    }
+    // End Method
+
+    public function clientchangeStatus(Request $request){
+        $client = Client::find($request->client_id);
+        $client->status = $request->status;
+        $client->save();
+        return response()->json(['success' => 'Status Change Successfully' ]);
+    }
 }
